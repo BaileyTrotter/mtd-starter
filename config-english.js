52592c21a714269b85b136dcfd073f6d72974959
BaileyTrotter <96272684+BaileyTrotter@users.noreply.github.com>
2022-06-10 18:11:03 +0000

    Deploying to gh-pages from  @ 1b8a9ae3aa6904959f2e7d358f593906ae9e4d8f 🚀

--- config-english.js
+++ config-english.js
@@ -1,1 +1,1 @@
-var config = {"L1": {"name": "English", "lettersInLanguage": ["d", "dh", "ddh", "a", "w"], "transducers": {"SFN-approx": [{"d": "\\ ddh"}, {"ddh": "\\ d"}]}}, "L2": {"name": "Nothern Tutchone"}, "build": "202206101745"}
+var config = {"L1": {"name": "English", "lettersInLanguage": ["d", "dh", "ddh", "a", "w"], "transducers": {"SFN-approx": [{"d": "\\ ddh"}, {"ddh": "\\ d"}]}}, "L2": {"name": "Nothern Tutchone"}, "build": "202206101811"}
